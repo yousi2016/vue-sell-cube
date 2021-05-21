9e2091f5d885740da72a1f48ffd17b522930482a
yousi2016 <dingyousi@gmail.com>
2021-05-21 19:40:49 +0800

    change shop-cart

--- src/components/shop-cart/shop-cart.vue
+++ src/components/shop-cart/shop-cart.vue
@@ -21,11 +21,12 @@
         </div>
       </div>
       <div class="ball-container">
-        <div v-for="(ball,index) in balls" :key="index">
+        <div v-for="(ball, index) in balls" :key='index'>
           <transition
             @before-enter="beforeDrop"
-            @enter="dropping"
-            @after-enter="afterDrop">
+            @enter = 'dropping'
+            @after-enter="afterDrop"
+          >
             <div class="ball" v-show="ball.show">
               <div class="inner inner-hook"></div>
             </div>
@@ -38,18 +39,17 @@
 
 <script>
   import Bubble from 'components/bubble/bubble'
-
   const BALL_LEN = 10
   const innerClsHook = 'inner-hook'
-
-  function createBalls() {
-    let balls = []
-    for (let i = 0; i < BALL_LEN; i++) {
-      balls.push({ show: false })
+  function createBalls () {
+    let ret = []
+    for (var i = 0; i < BALL_LEN; i++) {
+      ret.push({
+        show: false
+      })
     }
-    return balls
+    return ret
   }
-
   export default {
     name: 'shop-cart',
     props: {
@@ -76,14 +76,13 @@
         default: true
       }
     },
-    data() {
-      return {
-        balls: createBalls(),
-        listFold: this.fold
-      }
-    },
     created() {
       this.dropBalls = []
+    },
+    data() {
+      return {
+        balls: createBalls()
+      }
     },
     computed: {
       totalPrice() {
@@ -143,7 +142,7 @@
         e.stopPropagation()
       },
       drop(el) {
-        for (let i = 0; i < this.balls.length; i++) {
+        for (var i = 0; i < this.balls.length; i++) {
           const ball = this.balls[i]
           if (!ball.show) {
             ball.show = true
@@ -159,15 +158,15 @@
         const x = rect.left - 32
         const y = -(window.innerHeight - rect.top - 22)
         el.style.display = ''
-        el.style.transform = el.style.webkitTransform = `translate3d(0,${y}px,0)`
+        el.style.transform = el.style.webkitTransform = `translate3d(0, ${y}px, 0)`
         const inner = el.getElementsByClassName(innerClsHook)[0]
-        inner.style.transform = inner.style.webkitTransform = `translate3d(${x}px,0,0)`
+        inner.style.transform = inner.style.webkitTransform = `translate3d(${x}px, 0, 0)`
       },
       dropping(el, done) {
-        this._reflow = document.body.offsetHeight
-        el.style.transform = el.style.webkitTransform = `translate3d(0,0,0)`
+        this.reflow = document.body.offsetHeight
+        el.style.transform = el.style.webkitTransform = `translate3d(0, 0, 0)`
         const inner = el.getElementsByClassName(innerClsHook)[0]
-        inner.style.transform = inner.style.webkitTransform = `translate3d(0,0,0)`
+        inner.style.transform = inner.style.webkitTransform = `translate3d(0, 0, 0)`
         el.addEventListener('transitionend', done)
       },
       afterDrop(el) {
